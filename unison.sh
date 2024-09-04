614efa578532b9bfca361f813e87280e0de286a6
Gabin221 <serrurot19@gmail.com>
2024-09-04 18:02:58 +0200

    Update unison.sh

--- unison.sh
+++ unison.sh
@@ -7,17 +7,24 @@ REMOTE_BASE_DIR="/home/${REMOTE_USER}"  # Chemin de base du répertoire sur la m
 DISK_MOUNT_PATH="/media/<your name>/DISQUE ESSB"  # Chemin du disque dur externe
 
 # Dossiers à synchroniser
-DIRS=("exercism" "Documents" "bin" "Images" ".local/share/applications")
+DIRS=("exercism" "Documents" "bin" "Images")
+COMPLEX_DIRS=(".local/share/applications")
 
 # Fonction pour vérifier si une machine est accessible via SSH
 check_ssh() {
     echo "Vérification de l'accessibilité de la machine distante..."
     if ssh -o BatchMode=yes -o ConnectTimeout=5 ${REMOTE_USER}@${REMOTE_HOST} exit 2>/dev/null; then
         echo "La machine distante est accessible."
-        # Synchronisation
+        # Synchronisation des répertoires simples
         for DIR in "${DIRS[@]}"; do
             echo "Synchronisation du répertoire ~/${DIR}..."
-            unison ~/$(basename ${DIR}) ssh://${REMOTE_USER}@${REMOTE_HOST}//${REMOTE_BASE_DIR}/$(basename ${DIR}) -auto -batch -confirmbigdel=false -prefer newer -ignore 'Path env'
+            unison ~/${DIR} ssh://${REMOTE_USER}@${REMOTE_HOST}//${REMOTE_BASE_DIR}/$(basename ${DIR}) -auto -batch -confirmbigdel=false -prefer newer -ignore 'Path env' -ignorearchives
+        done
+
+        # Synchronisation des répertoires complexes
+        for DIR in "${COMPLEX_DIRS[@]}"; do
+            echo "Synchronisation du répertoire ~/${DIR}..."
+            sudo unison ~/${DIR} ssh://${REMOTE_USER}@${REMOTE_HOST}//${REMOTE_BASE_DIR}/${DIR} -auto -batch -confirmbigdel=false -prefer newer -ignore 'Path env' -ignorearchives
         done
     else
         echo "La machine distante n'est pas accessible. Synchronisation annulée."
@@ -29,10 +36,16 @@ check_disk() {
     echo "Vérification de l'accessibilité du disque dur..."
     if [ -d "${DISK_MOUNT_PATH}/unison" ]; then
         echo "Le disque dur est monté et accessible."
-        # Synchronisation avec le disque dur
+        # Synchronisation avec le disque dur des répertoires simples
         for DIR in "${DIRS[@]}"; do
             echo "Synchronisation avec le disque dur du répertoire ~/${DIR}..."
-            unison ~/$(basename ${DIR}) /media/<your name>/DISQUE\ ESSB/unison/$(basename ${DIR}) -auto -batch -confirmbigdel=false -prefer newer -ignore 'Path env' -perms 0
+            unison ~/${DIR} ${DISK_MOUNT_PATH}/unison/$(basename ${DIR}) -auto -batch -confirmbigdel=false -prefer newer -ignore 'Path env' -perms 0 -ignorearchives
+        done
+
+        # Synchronisation avec le disque dur des répertoires complexes
+        for DIR in "${COMPLEX_DIRS[@]}"; do
+            echo "Synchronisation avec le disque dur du répertoire ~/${DIR}..."
+            unison ~/${DIR} ${DISK_MOUNT_PATH}/unison/${DIR} -auto -batch -confirmbigdel=false -prefer newer -ignore 'Path env' -perms 0 -ignorearchives
         done
     else
         echo "Le disque dur n'est pas monté ou accessible. Synchronisation annulée."
@@ -46,3 +59,4 @@ check_ssh
 check_disk
 
 echo "Synchronisation terminée."
+
